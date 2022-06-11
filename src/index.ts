49f85fab8f43b50f066792a522c71857f3a4383e
Dessader <dessader.live@gmail.com>
2022-06-11 22:22:58 +0300

    Initialized project

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/*
+* App entry
+*/
+
+import "./scripts/app";
+import "./views/styles/app";
+
+/*
+* Import icons for generate SVG sprite
+*/
+
+import "./assets/images/icons/arrow-right.svg";
+import "./assets/images/icons/check.svg";
+import "./assets/images/icons/chevron-left.svg";
+import "./assets/images/icons/chevron-right.svg";
+import "./assets/images/icons/facebook.svg";
+import "./assets/images/icons/instagram.svg";
+import "./assets/images/icons/twitter.svg";
