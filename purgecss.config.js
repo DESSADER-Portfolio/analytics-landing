075cd55efe357ecd4cd4117035fc261055ab63a5
Dessader <dessader.live@gmail.com>
2022-06-18 21:14:47 +0300

    Added swiper classes to safelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -4,6 +4,6 @@ module.exports = {
   content: ["./build/*.html"],
   css: ["./build/styles/*.css"],
   output: ["./build/styles/"],
-  safelist: [/:active/, "no-scroll"],
+  safelist: [/:active/, /swiper/, "no-scroll"],
   defaultExtractor: content => content.match(/[A_Za-z0-9-_:/]+/g)
 };
